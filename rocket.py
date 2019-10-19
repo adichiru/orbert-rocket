ec8494b34ed13dc0fab1e1e52b0b6bfe6dca7d5d
Adi Chiru <adichiru@amazon.com>
2019-10-18 22:12:53 -0700

    First commit.

---
+++ rocket.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+
+def robi(count):
+  r = 0
+  while r < count:
+    print ("%i Robert" %(r))
+    r += 1
+
+
+#robi(10)
+
+
+
+
+
+def FalconHeavyTop():
+    top = "                     |"
+    print (top)
+    top2 = "                    /\\"
+    print (str(top2))
+    top3 = "                    | |"
+    print (str(top3))
+
+
+
+
+FalconHeavyTop()
